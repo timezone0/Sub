5937bd70818b87d572a3fd7a6f3226eee726e84e
timezone0 <213354478+timezone0@users.noreply.github.com>
2026-01-07 10:59:00 +0800

    Wed Jan  7 10:59:00 CST 2026

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# python scripts/xf.py
 python -u app-command.py --json "sub-timer.json"
 python -u app-command.py --json "sub.json"
 
+python -u app-command.py --json "sub-timer.json" --mihomo-dir mihomo-linux --mihomo-config mihomo-config/config-linux.json --singbox-dir singbox-linux --singbox-config singbox-config/config-linux.json
+python -u app-command.py --json "sub.json" --mihomo-dir mihomo-linux --mihomo-config mihomo-config/config-linux.json --singbox-dir singbox-linux --singbox-config singbox-config/config-linux.json
