2cdc1780ff49407cb4984d1b28cd11c46b110e06
timezone0 <213354478+timezone0@users.noreply.github.com>
2026-01-18 01:09:02 +0800

    Sun Jan 18 01:09:02 CST 2026

--- run_scripts2.sh
+++ run_scripts2.sh
@@ -2,4 +2,4 @@
 
 python generate-preferred-nodes/config/convert.py -c mihomo/tc.yaml -o generate-preferred-nodes/config/config.csv
 
-python generate-preferred-nodes/app.py generate-preferred-nodes/config/config.csv generate-preferred-nodes/config/config.txt generate-preferred-nodes/config/output.txt
+python generate-preferred-nodes/app.py --csv generate-preferred-nodes/config/config.csv --txt generate-preferred-nodes/config/config.txt --output generate-preferred-nodes/config/output.txt
