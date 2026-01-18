b886d161e7cc028d3399615a598ed0e78f5ac548
timezone0 <213354478+timezone0@users.noreply.github.com>
2026-01-18 22:37:17 +0800

    Sun Jan 18 22:37:17 CST 2026

--- run_scripts.sh
+++ run_scripts.sh
@@ -17,11 +17,11 @@ python -u app-command.py --json "sub-1.json" --mihomo-dir mihomo-windows --mihom
 python -u app-command.py --json "sub-2.json" --mihomo-dir mihomo-windows --mihomo-config scripts/mihomo-config/config-windows-open.yaml --singbox-dir singbox-windows --singbox-config scripts/singbox-config/config-windows-open.json
 
 #timer
-python -u app-command.py --name "timer" --url "mihomo/tc.yaml" --mihomo-dir mihomo --mihomo-config scripts/mihomo-config/config-android.yaml --singbox-dir singbox --singbox-config scripts/singbox-config/config-android.json
-python -u app-command.py --name "timer" --url "mihomo/tc.yaml" --mihomo-dir mihomo-linux --mihomo-config scripts/mihomo-config/config-linux.yaml --singbox-dir singbox-linux --singbox-config scripts/singbox-config/config-linux.json
-python -u app-command.py --name "timer" --url "mihomo/tc.yaml" --mihomo-dir mihomo-windows --mihomo-config scripts/mihomo-config/config-windows.yaml --singbox-dir singbox-windows --singbox-config scripts/singbox-config/config-windows.json
+python -u app-command.py --name "timer" --url "mihomo/8eb.yaml" --mihomo-dir mihomo --mihomo-config scripts/mihomo-config/config-android.yaml --singbox-dir singbox --singbox-config scripts/singbox-config/config-android.json
+python -u app-command.py --name "timer" --url "mihomo/8eb.yaml" --mihomo-dir mihomo-linux --mihomo-config scripts/mihomo-config/config-linux.yaml --singbox-dir singbox-linux --singbox-config scripts/singbox-config/config-linux.json
+python -u app-command.py --name "timer" --url "mihomo/8eb.yaml" --mihomo-dir mihomo-windows --mihomo-config scripts/mihomo-config/config-windows.yaml --singbox-dir singbox-windows --singbox-config scripts/singbox-config/config-windows.json
 
 #open
-python -u app-command.py --name "timer" --url "mihomo/tc.yaml" --mihomo-dir open --mihomo-config scripts/mihomo-config/config-android-open.yaml --singbox-dir open --singbox-config scripts/singbox-config/config-android-open.json
-python -u app-command.py --name "timer-linux" --url "mihomo/tc.yaml" --mihomo-dir open --mihomo-config scripts/mihomo-config/config-linux-open.yaml --singbox-dir open --singbox-config scripts/singbox-config/config-linux-open.json
-python -u app-command.py --name "timer-windows" --url "mihomo/tc.yaml" --mihomo-dir open --mihomo-config scripts/mihomo-config/config-windows-open.yaml --singbox-dir open --singbox-config scripts/singbox-config/config-windows-open.json
+python -u app-command.py --name "timer" --url "mihomo/8eb.yaml" --mihomo-dir open --mihomo-config scripts/mihomo-config/config-android-open.yaml --singbox-dir open --singbox-config scripts/singbox-config/config-android-open.json
+python -u app-command.py --name "timer-linux" --url "mihomo/8eb.yaml" --mihomo-dir open --mihomo-config scripts/mihomo-config/config-linux-open.yaml --singbox-dir open --singbox-config scripts/singbox-config/config-linux-open.json
+python -u app-command.py --name "timer-windows" --url "mihomo/8eb.yaml" --mihomo-dir open --mihomo-config scripts/mihomo-config/config-windows-open.yaml --singbox-dir open --singbox-config scripts/singbox-config/config-windows-open.json
